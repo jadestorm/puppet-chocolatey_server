6658335795a201cc0ebd939115b5d97f29197b3c
asif.shaikh <ripclaw_ffb@hotmail.com>
2016-09-24 22:14:33 -0700

    (GH-10) Add support for custom package folder
    Add additional parameters to modify the chocolatey repository package
    folder and set custom permissions. Update readme.

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,15 +21,20 @@
 #   }
 #
 # @param [String] port The port for the server website. Defaults to '80'.
+#
 # @param [String] server_package_source The chocolatey source that contains
 #   the `chocolatey.server` package. Defaults to
 #   'https://chocolatey.org/api/v2/'.
+#
 # @param [String] server_install_location The location to that the chocolatey
 #   server will be installed.  This is can be used if you are controlling
 #   the location that chocolatey packages are being installed via some other
 #   means. e.g. environment variable ChocolateyBinRoot.  Defaults to
 #   'C:\tools\chocolatey.server'
 class chocolatey_server (
+  $chocolatey_server_app_pool_name = $::chocolatey_server::params::chocolatey_server_app_pool_name,
+  $packages_folder = $::chocolatey_server::params::packages_folder,
+  $packages_folder_permissions = $::chocolatey_server::params::packages_folder_permissions,
   $port = $::chocolatey_server::params::service_port,
   $server_package_source = $::chocolatey_server::params::server_package_source,
   $server_install_location = $::chocolatey_server::params::server_install_location,
@@ -37,7 +42,7 @@ class chocolatey_server (
   require chocolatey
 
   $_chocolatey_server_location      = $server_install_location
-  $_chocolatey_server_app_pool_name = 'chocolatey.server'
+  $_chocolatey_server_app_pool_name = $chocolatey_server_app_pool_name
   $_chocolatey_server_app_port      = $port
   $_server_package_url              = $server_package_source
   $_is_windows_2008 = $::kernelmajversion ? {
@@ -67,38 +72,37 @@ class chocolatey_server (
   windowsfeature { "${_web_asp_net}":
   } ->
 
-  # remove default web site
-  iis::manage_site {'Default Web Site':
-    ensure    => absent,
-    site_path => 'any',
-    app_pool  => 'DefaultAppPool',
-    require   => Windowsfeature['Web-WebServer'],
-  } ->
-
-  # application in iis
+  # create application pool in iis
   iis::manage_app_pool { "${_chocolatey_server_app_pool_name}":
     enable_32_bit           => true,
     managed_runtime_version => 'v4.0',
   } ->
+
+  # create website for chocolatey server
   iis::manage_site {'chocolatey.server':
     site_path  => $_chocolatey_server_location,
     port       => "${_chocolatey_server_app_port}",
     ip_address => '*',
     app_pool   => "${_chocolatey_server_app_pool_name}",
     require    => Package['chocolatey.server'],
-  } ->
+  }
 
   # lock down web directory
   acl { "${_chocolatey_server_location}":
     purge                      => true,
     inherit_parent_permissions => false,
     permissions                => [
-      { identity => 'Administrators', rights => ['full'] },
-      { identity => 'IIS_IUSRS', rights => ['read'] },
-      { identity => 'IUSR', rights => ['read'] },
-      { identity => "IIS APPPOOL\\${_chocolatey_server_app_pool_name}", rights => ['read'] }
+      { identity => 'Administrators',
+      rights     => ['full'] },
+      { identity => 'IIS_IUSRS',
+      rights     => ['read'] },
+      { identity => 'IUSR',
+      rights     => ['read'] },
+      { identity => "IIS APPPOOL\\${_chocolatey_server_app_pool_name}",
+      rights     => ['read'] }
     ],
-    require                    => Package['chocolatey.server'],
+    require                    => [Iis::Manage_app_pool["${_chocolatey_server_app_pool_name}"],
+                                   Package['chocolatey.server']],
   } ->
   acl { "${_chocolatey_server_location}/App_Data":
     permissions => [
@@ -107,5 +111,54 @@ class chocolatey_server (
     ],
     require     => Package['chocolatey.server'],
   }
-  # technically you may only need IIS_IUSRS but I have not tested this yet.
+
+  # only set permissions if an alternate package folder is undefined
+  unless $packages_folder {
+    # ensure app_data folder is created
+    file { "${_chocolatey_server_location}/App_Data":
+      ensure => directory,
+    }
+
+    # ensure packages folder is created
+    file { "${_chocolatey_server_location}/App_Data/Packages":
+      ensure  => directory,
+      require => File["${_chocolatey_server_location}/App_Data"],
+    }
+
+    acl { "${_chocolatey_server_location}/App_Data":
+      permissions => $packages_folder_permissions,
+      require     => [Iis::Manage_app_pool["${_chocolatey_server_app_pool_name}"],
+                      File["${_chocolatey_server_location}/App_Data"]],
+    }
+    # technically you may only need IIS_IUSRS but I have not tested this yet.
+  }
+
+  # if a different folder for packages is specified, create a symlink
+  if $packages_folder {
+    # ensure app_data folder is created
+    file { "${_chocolatey_server_location}/App_Data":
+      ensure => directory,
+    }
+
+    # ensure packages folder is created
+    file { "${packages_folder}":
+      ensure => directory,
+    }
+
+    # create a symlink to the new packages folder, replacing the existing
+    # app_data folder
+    file { "${_chocolatey_server_location}/App_Data/Packages":
+      ensure  => link,
+      force   => true,
+      target  => $packages_folder,
+      require => File["${packages_folder}"],
+    }
+
+    # set permissions on the new packages folder
+    acl { "${packages_folder}":
+      permissions => $packages_folder_permissions,
+      require     => File["${packages_folder}"],
+    }
+  }
+
 }
